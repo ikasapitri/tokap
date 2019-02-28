bcbad2df5e9478b38753a5c698b36fa3e302e272
ikasapitri <ikasapitri02@gmail.com>
2019-02-28 09:54:04 +0700

    tokap eps-25

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -33,4 +33,15 @@ class KategoriController extends Controller
             return back()->with('result','fail')->withInput();
         }
     }
+
+    public function edit($id)
+    {
+        $data = kategori::where('id',$id)->first();
+        return view('admin.pages.kategori.edit',['rc'=>$data]);
+    }
+
+    public function update(Request $req)
+    {
+        return 'Fungsi Update';
+    }
 }
